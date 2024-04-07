65f60c3df1a3fb434e1a98df2d9ec86047d79740
jesse <31576321+agentjesse@users.noreply.github.com>
2024-04-06 23:11:40 -0400

    progress in makeTree fn

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
 /* Next task:
-- makeTreeMidNode needs to return the level-0 tree root node to its caller, the getTreeFromArr fn. build tree fully from objects attached to root node before returning it
+- makeTree needs to return the level-0 tree root node to its caller, the getTreeFromArr fn. build tree fully into level-0 node before returning it
 */
 
 //For Node.js, when importing local modules, include the file extension in the import statement.
-import { logToConsole as lg, tableToConsole as tb } from './logger.js'; //shorthand loggers
+import { logToConsole as lg, objectToString as ots } from './logger.js'; //shorthand loggers
 
-const makeNode = (data, left = null, right = null)=> ({ data, left, right });
+const makeNode = (data, left = null, right = null)=> ( { data, left, right } );
 
-const makeTreeMidNode = (cleanArr)=> {
+//this fn takes a sorted array and makes a parent node from the middle element. as it is recursively calling itself with a sub array (start to mid, mid to end), eventually a base condition should be reached to return null instead of a node.
+const makeTree = (cleanArr)=> {
+  lg(cleanArr); //debugging
+  //base case below might be wrong, check....
   if ( !Array.isArray(cleanArr) ) return null;
-  lg(cleanArr);
+  //make parent node from array's middle element. Math.floor is optional
+  const midIndex = Math.floor( cleanArr.length / 2 );
+  const root = makeNode( cleanArr[midIndex] );
+  //make parent node's left subtree root...might need to check for existence
+  root.left = makeTree(  );
 
-  return 'testData, return level-0 tree root to getTreeFromArr fn...';
+  return root;
 };
 
 //fn to clean array and return tree root node
@@ -19,7 +26,7 @@ const getTreeFromArr = (dirtyArr)=> {
   const cleanArr = dirtyArr.sort( (a, b)=> a - b ) //sort array
     //remove dupes. Filter skips keeping current value it's equal to next one
     .filter( (currentVal, currentIndex)=> !( currentVal === dirtyArr[currentIndex + 1] ) );
-  return makeTreeMidNode( cleanArr );
+  return makeTree( cleanArr );
 };
 
 //fn to print a binary tree from Odin. Provide root node, optional prefix like \n
@@ -29,7 +36,7 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
   lg(`${prefix}${isLeft ? '└─ ' : '┌─ '}${node.data}`);
   node.left !== null && prettyPrint(node.left,`${prefix}${isLeft ? '   ' : '│  '}`,true);
 };
-//---prettyPrint fn example
+//---prettyPrint fn example ... or use JSON.stringify with '\t'
 const testTree = {
   data: 4,
   left: {
@@ -42,5 +49,6 @@ const testTree = {
 // prettyPrint(testTree);
 
 //testing
-const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-lg( getTreeFromArr( testArr ) );
+// const testArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+const testArr = [3, 1, 2];
+lg( `level-0 tree root:\n${ ots( getTreeFromArr( testArr ) ) }` );
