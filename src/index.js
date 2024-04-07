38b7c2035eddfa2f085a581181b524dda60da1d3
jesse <31576321+agentjesse@users.noreply.github.com>
2024-04-07 07:21:28 -0400

    level/pre/in/post order traversal fns added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 /* Next task:
-- add levelOrder(callback) function to traverse bst in level order AND provide each node as an argument to the callback to do something.
-Return an array of values if no callback provided.
+-
 */
 
 //For Node.js, when importing local modules, include the file extension in the import statement.
@@ -46,26 +45,104 @@ const prettyPrint = (node, prefix = '', isLeft = true) => {
 //fn to wrap a BST with management methods. Pass in a BST level-0 root node
 const makeManagedBST = (level0RootNode)=> {
   //management fns
+  //fn to traverse BST in level order and return an array of each visited node's
+  //data, or to invoke a callback for each node
   const levelOrder = (callback = null)=> {
-    //initialize a queue data structure with level-0 root node
-    const q = makeQueue();
-    q.enqueue( level0RootNode );
+    const q = makeQueue(); //make queue
+    q.enqueue( level0RootNode ); //enqueue level-0 root node
+    const result = []; //empty array for results
     //while q size > 0: dequeue node, read its data, enqueue its children
     while ( q.getSize() ) {
       //dequeue() returns queue node's data, which is a BST node in this case
       const dequeuedNode = q.dequeue();
       //use data, enqueue existing children
-      //if no callback, print data
-      lg( dequeuedNode.data );
+      if (callback) { //callback exists? invoke it with data
+        callback( dequeuedNode.data );
+      } else { //no callback? save data
+        result.push( dequeuedNode.data );
+      }
       if (dequeuedNode.left) q.enqueue( dequeuedNode.left );
       if (dequeuedNode.right) q.enqueue( dequeuedNode.right );
     }
+    if (!callback) return result; //no callback? return result[]
+  };
+
+  //fn to traverse BST in preorder and return an array of each visited node's
+  //data, or to invoke a callback for each node
+  const preOrder = (callback = null)=> {
+    const result = []; //empty array for results
+    //recursive fn for traversal
+    const traverseFromNode = (node)=> {
+      if (!node) return; //base case, expected a node but got null
+      //use data
+      if (callback) { //callback exists? invoke it with data
+        callback( node.data );
+      } else { //no callback? save data
+        result.push( node.data );
+      }
+      //traverse to left child recursively
+      traverseFromNode( node.left );
+      //traverse to right child recursively
+      traverseFromNode( node.right );
+    };
+    //start the recursive traversal with level-0 root node
+    traverseFromNode( level0RootNode );
+    if (!callback) return result; //no callback? return result[]
+  };
+
+  //fn to traverse BST in inorder and return an array of each visited node's
+  //data, or to invoke a callback for each node
+  const inOrder = (callback = null)=> {
+    const result = []; //empty array for results
+    //recursive fn for traversal
+    const traverseFromNode = (node)=> {
+      if (!node) return; //base case, expected a node but got null
+      //traverse to left child recursively
+      traverseFromNode( node.left );
+      //use data
+      if (callback) { //callback exists? invoke it with data
+        callback( node.data );
+      } else { //no callback? save data
+        result.push( node.data );
+      }
+      //traverse to right child recursively
+      traverseFromNode( node.right );
+    };
+    //start the recursive traversal with level-0 root node
+    traverseFromNode( level0RootNode );
+    if (!callback) return result; //no callback? return result[]
+  };
+
+  //fn to traverse BST in postorder and return an array of each visited node's
+  //data, or to invoke a callback for each node
+  const postOrder = (callback = null)=> {
+    const result = []; //empty array for results
+    //recursive fn for traversal
+    const traverseFromNode = (node)=> {
+      if (!node) return; //base case, expected a node but got null
+      //traverse to left child recursively
+      traverseFromNode( node.left );
+      //traverse to right child recursively
+      traverseFromNode( node.right );
+      //use data
+      if (callback) { //callback exists? invoke it with data
+        callback( node.data );
+      } else { //no callback? save data
+        result.push( node.data );
+      }
+    };
+    //start the recursive traversal with level-0 root node
+    traverseFromNode( level0RootNode );
+    if (!callback) return result; //no callback? return result[]
   };
 
   return {
     level0RootNode,
     prettyPrintBSD: ()=> prettyPrint(level0RootNode),
-    levelOrder
+    levelOrder,
+    preOrder,
+    inOrder,
+    postOrder
   };
 };
 
@@ -76,4 +153,27 @@ const testArr = [3, 1, 2];
 const pureBST = makeTree( processArr( testArr ) );
 const managedBST = makeManagedBST( pureBST );
 managedBST.prettyPrintBSD();
-managedBST.levelOrder();
+
+//level order traversal with no callback returns data array
+// lg( 'level order traversal:\b');
+// lg( managedBST.levelOrder() );
+//level order traversal with callback to invoke for each node
+// lg( managedBST.levelOrder(lg) );
+
+//preorder traversal with no callback returns data array
+lg( 'preorder traversal:\b');
+lg( managedBST.preOrder() );
+//preorder traversal with callback to invoke for each node
+// lg( managedBST.preOrder(lg) );
+
+//inorder traversal with no callback returns data array
+// lg( 'inorder traversal:\b');
+// lg( managedBST.inOrder() );
+//inorder traversal with callback to invoke for each node
+// lg( managedBST.inOrder(lg) );
+
+//postorder traversal with no callback returns data array
+// lg( 'postorder traversal:\b');
+// lg( managedBST.postOrder() );
+//postorder traversal with callback to invoke for each node
+// lg( managedBST.postOrder(lg) );
